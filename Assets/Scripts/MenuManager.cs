32ef06fbdb3f208fb3df74beb2eeef5a58e4de90
Tadeas Paule <tpaule.personal@gmail.com>
2020-04-18 12:48:47 +0200

    Basic main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Player player;
+
+    void Start()
+    {
+        player.ToggleFreezeMovement(true);
+    }
+    
+    public void ClickedPlay()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ClickedSoundsToggle()
+    {
+
+    }
+
+    public void ClickedQuit()
+    {
+        Application.Quit();
+    }
+}
