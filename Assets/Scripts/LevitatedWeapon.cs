536da2870e624d076f7243220108bdc5100c751b
Tadeas Paule <tpaule.personal@gmail.com>
2020-04-18 17:25:47 +0200

    initial visuals for player/stroller/levitron

--- Assets/Scripts/LevitatedWeapon.cs
+++ Assets/Scripts/LevitatedWeapon.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class LevitatedWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float degs = 0f;
+    float rotateSpeed = 15f;
+
+    public Rigidbody2D rb;
 
     // Update is called once per frame
     void Update()
     {
-        
+        degs += (Time.deltaTime * rotateSpeed) + (rb.velocity.magnitude / 15f) * 20f;
+        transform.rotation = Quaternion.Euler(0f,0f,degs);
     }
 }
