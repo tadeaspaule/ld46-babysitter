2bc601f0d403b1ffbd4ab49f8786350107e4d7b2
Tadeas Paule <tpaule.personal@gmail.com>
2020-04-18 08:11:23 +0200

    Initial commit, basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float moveSpeed = 7f;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.A)) {
+            movement += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            movement += Vector3.right;
+        }
+        rb.velocity = movement * moveSpeed;
+    }
+}
