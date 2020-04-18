780d9196597ec5b0283a352f0c154cf965a9f98e
Tadeas Paule <tpaule.personal@gmail.com>
2020-04-18 15:33:14 +0200

    Refactor staticshooter removed the direction variable, now shoots below itself, taking into account the shooters rotation

--- Assets/Scripts/StaticShooter.cs
+++ Assets/Scripts/StaticShooter.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class StaticShooter : ShooterBase
 {
-    public Vector2 shootDirection;
-
     public override void Shoot()
     {
         GameObject bullet = Instantiate(bulletPrefab,Vector3.zero,Quaternion.identity,FindObjectOfType<BulletHolder>().transform);
         bullet.transform.position = transform.position;
-        bullet.GetComponent<Rigidbody2D>().velocity = shootDirection.normalized * shootSpeed;
+        bullet.GetComponent<Rigidbody2D>().velocity = GetShootDirection() * shootSpeed;
+    }
+
+    Vector2 GetShootDirection()
+    {
+        float rads = (Mathf.PI*2) * (transform.rotation.eulerAngles.z / 360f) - Mathf.PI/2;
+        return new Vector2(
+            Mathf.Cos(rads),
+            Mathf.Sin(rads)
+        );
     }
 }
