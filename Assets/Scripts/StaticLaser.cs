af338df789d3640f67d9e803e741cd1e9d186287
Tadeas Paule <tpaule.personal@gmail.com>
2020-04-19 13:04:26 +0200

    static laser prefab

---
+++ Assets/Scripts/StaticLaser.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticLaser : ShooterBase
+{
+    public SpriteRenderer laserSR;
+    public BoxCollider2D laserBox2D;
+    public float shootDelay = 0.6f;
+    float laserDuration = 0.1f;
+
+    float hiddenLaser = 0f;
+    float visibleLaser = 1f;
+    float warningLaser = 0.3f;
+
+    protected new void CallWhenFrozen()
+    {
+        Debug.Log("LASR");
+        StopAllCoroutines();
+    }
+    
+    
+    public override void Shoot()
+    {
+        SetAlpha(warningLaser);
+        StartCoroutine(DelayedShot());
+    }
+
+    IEnumerator DelayedShot()
+    {
+        yield return new WaitForSeconds(shootDelay);
+        SetAlpha(visibleLaser);
+        laserBox2D.enabled = true;
+        StartCoroutine(DelayedHideLaser());
+    }
+
+    IEnumerator DelayedHideLaser()
+    {
+        yield return new WaitForSeconds(laserDuration);
+        SetAlpha(hiddenLaser);
+        laserBox2D.enabled = false;
+    }
+
+    void SetAlpha(float alpha)
+    {        
+        laserSR.color = new Color(laserSR.color.r,laserSR.color.g,laserSR.color.b,alpha);
+    }
+}
